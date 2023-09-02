2253c960ca81bbcb5f9c4ed9fb88e956701b3ab9
DanikaVeresha <109421605+DariaVeresha@users.noreply.github.com>
2023-09-02 17:04:52 +0300

    Add files via upload

---
+++ sms_insta.py
@@ -0,0 +1,3 @@
+textbox = 'Hi, how do you feel? Please click this link: ' \
+          'https://forumsmile.net/pic59590-solnechnyy-privet-otlichnogo-nastroyeniya.html ' \
+          'Have a nice day!Thanks a lot, well done!'
